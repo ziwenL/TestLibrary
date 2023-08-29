9f581f69623d2ac638e21967f0b8d29d1072ba7f
lanziwen <lanziwen@rt.com>
2023-08-29 15:07:17 +0800

    first

---
+++ app/src/main/java/com/example/testlibrary/MainClass.kt
@@ -0,0 +1,9 @@
+package com.example.testlibrary
+
+/**
+ * @author ZiWen
+ * CreateDate:2023/8/29
+ * Description:
+ */
+class MainClass {
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/LibraryClass.kt
@@ -0,0 +1,10 @@
+package com.example.mylibrary
+
+/**
+ * @author ZiWen
+ * CreateDate:2023/8/29
+ * Description:
+ */
+class LibraryClass {
+
+}
