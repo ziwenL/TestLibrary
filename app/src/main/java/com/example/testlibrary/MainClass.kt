78c318caea1b2335d82cd79ae5e80eec6b5968c3
lanziwen <lanziwen@rt.com>
2023-08-29 15:19:56 +0800

    first

--- app/src/main/java/com/example/testlibrary/MainClass.kt
+++ app/src/main/java/com/example/testlibrary/MainClass.kt
@@ -1,9 +1,14 @@
 package com.example.testlibrary
 
+import android.util.Log
+
 /**
  * @author ZiWen
  * CreateDate:2023/8/29
  * Description:
  */
 class MainClass {
+    fun test() {
+        Log.e("TAG", "")
+    }
 }
--- mylibrary/src/main/java/com/example/mylibrary/LibraryClass.kt
+++ mylibrary/src/main/java/com/example/mylibrary/LibraryClass.kt
@@ -1,5 +1,7 @@
 package com.example.mylibrary
 
+import android.util.Log
+
 /**
  * @author ZiWen
  * CreateDate:2023/8/29
@@ -7,4 +9,7 @@ package com.example.mylibrary
  */
 class LibraryClass {
 
+    fun test() {
+        Log.e("TAG", "")
+    }
 }
